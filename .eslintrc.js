7c4535bc28f688c3b37c7023bee359514e5d8008
syedemz <syedemad11@gmail.com>
2026-05-03 17:34:10 +0200

    phase 1 story 1.5: configure Prettier
    - prettier@3.8.3 and eslint-config-prettier@10.1.8 installed as devDependencies
    - .prettierrc: singleQuote:true, trailingComma:"all", printWidth:100, semi:true
    - .prettierignore: excludes node_modules, .expo, android, ios, lockfiles, *.md
    - format and format:check npm scripts added to package.json
    - 'prettier' added as final entry to .eslintrc.js extends (disables ESLint formatting conflicts)
    - npx prettier --check . exits 0; all code files already consistent with chosen settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 // The tsdoc/syntax rule is registered manually via the plugins + rules fields below.
 // See story 1.4 notes in implementationplan/phase-1-bootstrap.md for the audit trail.
 module.exports = {
-  extends: ['expo', 'plugin:@typescript-eslint/recommended'],
+  extends: ['expo', 'plugin:@typescript-eslint/recommended', 'prettier'],
   plugins: ['tsdoc'],
   rules: {
     'tsdoc/syntax': 'warn',
